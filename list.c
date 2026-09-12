19f6b3d235327f999c4a9130d8e0e93473144691
LAPTOP-HIBQ8KAL\sander <sanderlahoz123@gmail.com>
2026-09-12 13:16:34 +0200

    Testing out the old semantic

--- list.c
+++ list.c
@@ -167,9 +167,13 @@ void list_push_front(list_t *l, list_elem_t *e)
 
   e->next = l->head;
   l->head = e;
-  l->size++;
 
-  return;
+  if (l->tail == NULL)
+  {
+    l->tail = e;
+  }
+
+  l->size++;
 }
 
 /**
