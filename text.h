2cb850c8c6bf407bf11e16eba4eb2e7f4d2e208b
LAPTOP-HIBQ8KAL\sander <sanderlahoz123@gmail.com>
2026-09-17 13:19:52 +0200

    Bug

--- text.h
+++ text.h
@@ -3,7 +3,7 @@
  * @brief Header file for implementing operations on list of strings.
  *
  * Lab for practicing basic C programming skills.
- * 
+ *
  * Developed for ITU course Operating Systems and C.
  * A fork of the C programming lab developed at CMU for courses
  * 15-213/18-213/15-513 by R. E. Bryant, 2017 (extended w/ strings 2018)
@@ -16,7 +16,7 @@
  * Now that you are done with `list.h` and `list.c`:
  *
  * The testing facility calls the following functions from `text.c` :
- * 
+ *
  * - text_list_free
  * - text_list_push_front
  * - text_list_push_back
@@ -40,26 +40,27 @@
 /**
  * @brief A text is a string that can be part of one linked list.
  */
-struct text {
-  char* value;               // Pointer to a string (sequence of characters).
-  list_elem_t elem;          // List element.
+struct text
+{
+  char *value;      // Pointer to a string (sequence of characters).
+  list_elem_t elem; // List element.
 };
 
-typedef struct text text_t;  // a type synonym.
+typedef struct text text_t; // a type synonym.
 
 /**** Text API ****************************************************************/
 
 /* Get a pointer to the text_t which has e as its elem. */
-text_t* text_list_entry ( list_elem_t* e );
+text_t *text_list_entry(list_elem_t *e);
 
 /* Attempt to insert a text_t with value s at head of l. */
-bool text_list_push_front ( list_t* l, const char* s );
+bool text_list_push_front(list_t *l, const char *s);
 
 /* Attempt to insert a text_t with value s at tail of l. */
-bool text_list_push_back ( list_t* l, const char* s );
+bool text_list_push_back(list_t *l, const char *s);
 
 /* Attempt to remove text_t at head of l, writing its value to s */
-bool text_list_pop_front ( list_t* l, char* s, size_t n );
+bool text_list_pop_front(list_t *l, char *s, size_t n);
 
 /* Free ALL storage used by l. */
-void text_list_free ( list_t* l );
+void text_list_free(list_t *l);
